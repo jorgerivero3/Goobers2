06520c03b8a548633943f03aaa75eaeb63891a15
Jack Schaeffer <jackschaeffer@outlook.com>
2019-04-26 17:36:45 -0500

    updated CoreData

--- Goobers2/newPartyView.swift
+++ Goobers2/newPartyView.swift
@@ -10,43 +10,50 @@ import UIKit
 import CoreData
 
 class newPartyView: UIViewController {
+
+    
+    @IBOutlet weak var pName: UITextField!
+    @IBOutlet weak var pDesc: UITextField!
+    @IBOutlet weak var pDate: UIDatePicker!
+    @IBOutlet weak var pPlace: UITextField!
+    
+    var parties: [NSManagedObject] = []
     
     
-    
-    
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-    return
-    }
-    
-    let managedContext = appDelegate.persistentContainer.viewContext
-    
-    let entity = NSEntityDescription.entity(forEntityName: "Adventurer", in: managedContext)!
-    
-    let newChar = NSManagedObject(entity: entity, insertInto: managedContext)
-    
-    newChar.setValue(newID, forKey: "id")
-    newID += 1
-    newChar.setValue(charName?.text!, forKeyPath: "name")
-    newChar.setValue(charClass?.text!, forKeyPath: "profession")
-    newChar.setValue(health, forKeyPath: "hp")
-    newChar.setValue(attack, forKeyPath: "ap")
-    newChar.setValue(level, forKeyPath: "level")
-    newChar.setValue(selectedImage, forKeyPath: "portrait")
-    
-    do {
-    try managedContext.save()
-    adventurers.append(newChar)
-    print("Saved character \(String(describing: charName?.text!))")
-    } catch let error as NSError {
-    print("Failed to save, \(error), \(error.userInfo)")
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
+    
+    @IBAction func newParty(_ sender: UIButton) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+        return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Party", in: managedContext)!
+        let newParty = NSManagedObject(entity: entity, insertInto: managedContext)
+        print("a")
+        newParty.setValue(pName?.text, forKey: "name")
+        newParty.setValue(pDesc?.text, forKey: "desc")
+        newParty.setValue(pDate?.date, forKey: "date")
+        newParty.setValue(pPlace?.text, forKey: "place")
+        
+//        let newParty = NSManagedObject(entity: entity, insertInto: managedContext)
+//
+//        newParty.setValue(pName?.text!, forKeyPath: "name")
+//
+        do {
+        try managedContext.save()
+        parties.append(newParty)
+        print("Saved character \(String(describing: pName?.text!))")
+            
+            
+        } catch let error as NSError {
+        print("Failed to save, \(error), \(error.userInfo)")
+        }
+    
+    }
 }
